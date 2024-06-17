b4cb9b6412e7689c977b0de97e050cbc64c91019
mayor167 <adenijimayokun@gmail.com>
2024-06-17 09:14:47 +0100

    VIDEO3 done

---
+++ WEEK1/VIDEO3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if(true){?>
+    <p>Some HTML text.</p>
+    <?php
+    }
+    //COMMENTING YOUR CODE
+    #not include in the code
+     echo "<h1>hello World, I'm a great programmer</h1>"
+    ?>
+    <h1><?php echo "My name is ADENIJI Oyeyemi" 
+    /*
+    This is multiple line comments. Take note
+    */
+    ?></h1>
+    
+    
+</body>
+</html>
