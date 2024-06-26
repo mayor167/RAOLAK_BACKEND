8f70317046a04cad323edc3e59dad0849878e8f8
mayor167 <adenijimayokun@gmail.com>
2024-06-26 21:31:19 +0100

    done

---
+++ WEEK2/VIDEO12/index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User-defined Function in PHP</title>
+</head>
+<body>
+    <?php 
+    //creating my own function in php
+    function greetCustomer(){
+        echo "Hello World"."<br>";
+        echo "World is a subset of Nature"."<br>";
+    }
+    greetCustomer();//calling the function here do its work.
+    function moreGreet(){
+        return greetCustomer();
+    }
+    moreGreet();
+    //print_r($storeMoreGreet);
+    $c = 10;
+    $d = 10;
+    function add(int $a,int $b){//defined function add with parameters/or placeholder a,b
+         return $a + $b;
+         
+    }
+    $sum = add(4,6); //invoking/calling the function add with 4,6 as inputs to save it result in variable $sum
+    echo $sum."<br>";
+    function add2(){
+         global $c; 
+          global $d;
+          return $c + $d;
+    }
+    echo add2()."<br>";
+    ?>
+</body>
+</html> 
