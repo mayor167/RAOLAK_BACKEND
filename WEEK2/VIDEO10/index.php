a29d865b953f4decebd2d73d797377f9bb942adc
mayor167 <adenijimayokun@gmail.com>
2024-07-04 02:00:19 +0100

    done

--- WEEK2/VIDEO10/index.php
+++ WEEK2/VIDEO10/index.php
@@ -16,12 +16,12 @@
             "England",  // 2
           ];// $countries, $fruits, $cars are an indexed arrays
  //assessing array using index number
-            echo $countries[1]."<br>";
-           // unset($countries[1]);
-            print_r($countries);
-            $countries[] = "Russian";
-            array_push($countries, "canada");
-            print_r($countries);
+          //   echo $countries[1]."<br>";
+          //  // unset($countries[1]);
+          //   print_r($countries);
+          //   $countries[] = "Russian";
+          //   array_push($countries, "canada");
+          //   print_r($countries);
            //print_r($countries);
           $choresTimetable =[
                     "Laundry" => "Seyi",
@@ -29,7 +29,23 @@
                     "Cooking" => "Temi",
                     "Mopping" => "Kenny"
           ];// $choresTimetable is an assosative array that uses keys to access its elements.
+$person = [
+  'name' => 'Mr akin',
+  'occupation' => 'Engineer',
+  'number_of_wives' => 5,
+  'number_of_friends' => 1000
+];
 
+$person['number_of_meals'] = 3;
+// array_map(function($arr) {
+//   print_r($arr);
+//   echo "<br>";
+// }, $person);
+array_pop($person);
+array_pop($person);
+
+print_r($person);
+exit;
 //assessing array using keys
                 echo $choresTimetable["Laundry"]. "<br>";
                 print_r($choresTimetable)."<br>";
